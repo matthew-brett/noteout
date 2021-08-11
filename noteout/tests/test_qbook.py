3758e737494c915c10c24a80252ca1fa0efc67ae
Matthew Brett <matthew.brett@gmail.com>
2021-08-11 23:46:18 +0100

    Test for output from meta filter

--- noteout/tests/test_qbook.py
+++ noteout/tests/test_qbook.py
@@ -10,6 +10,9 @@ QBOOK_PATH = Path(__file__).parent.joinpath('qbook')
 
 
 def test_qbook_render(tmp_path):
+    # Test qbook build.
+    # Output from write_meta filter.
+    metas = set([f'meta_{i:03d}.json' for i in range(4)])
     for lang, exp_ext in (('Python', 'ipynb'),
                           ('R', 'Rmd')):
         tmp_qbook = tmp_path / f'{lang}_qbook'
@@ -18,6 +21,10 @@ def test_qbook_render(tmp_path):
             fobj.write(f'edition: {lang.lower()}\n'
                        f'lang: {lang}')
         run(['quarto', 'render', '.'], cwd=tmp_qbook)
+        # Check meta files from write_meta
+        source_listing = listdir(tmp_qbook)
+        assert metas <= set(source_listing)
+        # Check book generation.
         generated = listdir(tmp_qbook / '_book')
         assert set(['index.html',
                     'my_notebook.' + exp_ext,
