d2aa664963b36741451526bc500383fd0c617fe3
Matthew Brett <matthew.brett@gmail.com>
2024-08-21 11:18:47 +0100

    Patch for Quarto JSON output

--- noteout/__init__.py
+++ noteout/__init__.py
@@ -2,3 +2,10 @@
 """
 
 __version__ = '0.0.3a7'
+
+# Patch Panflute for Quarto RawBlocks:
+# https://github.com/sergiocorreia/panflute/pull/251
+
+import panflute as _pf
+
+_pf.elements.RAW_BLOCKS.add('latex-merge')
