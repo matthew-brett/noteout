ee6cf834e82c3289998615a7e914b42aa269323d
Matthew Brett <matthew.brett@gmail.com>
2024-11-07 22:01:20 +0000

    Finish adding links module and test.

--- src/noteout/add_notebook_links.py
+++ src/noteout/add_notebook_links.py
@@ -19,13 +19,29 @@ download links instead of notebook download links).
 """
 
 from pathlib import Path
+import re
+import zipfile
 
+import jupytext as jpt
 import panflute as pf
 
 from noteout.nutils import FilterError, fmt2fmt
 
 _REQUIRED_NOTEOUT_KEYS = ('book-url-root', 'nb-dir', 'nb-format')
 _REQUIRED_MARKER_KEYS = ('name', 'title')
+
+# Find data read
+READ_RE = re.compile(
+    r'''^\s*
+    \w+\s*(=|<-)\s*
+    (pd\.)*read[._]\w+\(
+    ['"]
+    (?P<fname>.*?)
+    ['"]
+    \)
+    ''',
+    flags=re.MULTILINE | re.VERBOSE)
+
 
 
 def prepare(doc):
@@ -35,8 +51,11 @@ def prepare(doc):
             raise FilterError(f'noteout.{key} must be defined in metadata')
     if params.get('url-nb-suffix') is None:
         params['url-nb-suffix'] = '.' + params.get('nb-format', 'ipynb')
-    params['out_format' = doc.get_metadata('quarto-doc-params.out_format')
-    params['nb_out_path'] = 
+    params['interact-url'] = '/' + params['interact-url'].strip('/')
+    qdp = doc.get_metadata('quarto-doc-params', {})
+    params['out_format'] = qdp.get('out_format')
+    params['out_path'] = Path(qdp.get('output_directory', '.'))
+    params['nb_out_path'] = params['out_path'] / params['nb-dir']
     doc._params = params
 
 
@@ -44,23 +63,96 @@ def finalize(doc):
     del doc._params
 
 
-def get_process_nb(elem, doc):
-    return Path('foo.ipynb')
+def find_data_files(nb):
+    """ Detect data files read within notebook
+
+    Parameters
+    ----------
+    nb : dict
+        Notebook.
+
+    Returns
+    -------
+    out_files : list
+        List of detected filenames.  These will be relative to the path assumed
+        by the `nb`.
+    """
+    out_fnames = []
+    for cell in nb['cells']:
+        if cell['cell_type'] != 'code':
+            continue
+        for m in READ_RE.finditer(cell['source']):
+            out_fnames.append(m.groups()[-1])
+    return sorted(set(out_fnames))
 
 
-def get_nb_links(elem, doc, dl_suffix):
+def write_data_files(in_path, data_files, out_path):
+    """ Write data files `data_files` to path `out_path`
+
+    Parameters
+    ----------
+    in_path : :class:`Path`
+        Directory containing input `data_files`.
+    data_files : sequence of str
+        Filenames giving paths to data files.
+    out_path : :class:`Path`
+        Output path.
+
+    Returns
+    -------
+    out_paths : list
+        List of Paths of written files.
+    """
+    out_paths = []
+    for data_fname in data_files:
+        data_out_path = out_path / data_fname
+        data_out_path.parent.mkdir(parents=True, exist_ok=True)
+        data_out_path.write_text((in_path / data_fname).read_text())
+        out_paths.append(data_out_path)
+    return out_paths
+
+
+def write_zip(paths, out_path):
+    out_zip_path = paths[0].with_suffix('.zip')
+    with zipfile.ZipFile(out_zip_path, "w") as zf:
+        for path in paths:
+            zf.write(path, str(path.relative_to(out_path)))
+    return out_zip_path
+
+
+def get_make_dl(name, doc):
+    p = doc._params
+    out_path = p['nb_out_path']
+    nb_path =  out_path / f'{name}.{p["nb-format"]}'
+    if not nb_path.is_file():
+        raise FilterError(f'"{nb_path}" does not exist')
+    nb = jpt.reads(nb_path.read_text(), 'Rmd')
+    if (dfs := find_data_files(nb)):
+        out_data_files = write_data_files(Path(), dfs, out_path)
+        return write_zip([nb_path] + out_data_files, out_path), len(dfs)
+    return nb_path, 0
+
+
+def get_nb_links(elem, doc, dl_path, n_dfs):
     params = doc._params.copy()
     params.update(elem.attributes)
+    out_dir = params['out_path']
+    params['dl_fname'] = str(dl_path.relative_to(out_dir))
+    params['dl_txt'] = ('notebook' if n_dfs == 0 else
+                        ('zip with notebook + data file' +
+                         ('s' if n_dfs > 1 else '')))
+    params['inter_url'] = ('{interact-url}{name}{url-nb-suffix}'
+                           .format(**params))
     if doc.get_metadata('quarto-doc-params.out_format') == 'html':
         txt = '''\
 <div class="nb-links">
-<a class="notebook-link" href="{nb-dir}/{name}.{nb-format}">Download notebook</a>
-<a class="interact-button" href="{interact-url}{name}{url-nb-suffix}">Interact</a>\n')
+<a class="notebook-link" href="{dl_fname}">Download {dl_txt}</a>
+<a class="interact-button" href="{inter_url}">Interact</a>\n')
 </div>'''.format(**params)
     else:  # Generic format.
         txt = '''\
-* [Download notebook]({book-url-root}/{nb-dir}/{name}.{nb-format})
-* [Interact]({book-url-root}{interact-url}{name}{url-nb-suffix})
+* [Download notebook]({book-url-root}/{dl_fname})
+* [Interact]({book-url-root}{inter_url})
 '''.format(**params)
     return fmt2fmt(txt, out_fmt='panflute').content
 
@@ -71,7 +163,7 @@ def action(elem, doc):
     for key in _REQUIRED_MARKER_KEYS:
         if not key in elem.attributes:
             raise FilterError(f'{key} must be defined in nb-start element')
-    nb_pth = get_process_nb(elem, doc)
+    dl_path, n_dfs = get_make_dl(elem.attributes['name'], doc)
     elem.content = (list(elem.content) +
-                    list(get_nb_links(elem, doc, nb_pth.suffix)))
+                    list(get_nb_links(elem, doc, dl_path, n_dfs)))
     return elem
--- src/noteout/tests/test_add_notebook_links.py
+++ src/noteout/tests/test_add_notebook_links.py
@@ -7,6 +7,9 @@
 * For notebooks with data files, the Download link should be to a zip archive.
 """
 
+from pathlib import Path
+
+import jupytext as jpt
 import noteout.add_notebook_links as anbL
 
 from noteout.nutils import fmt2fmt, FilterError, filter_doc
@@ -75,7 +78,7 @@ The notebook `a_notebook` starts at @nte-a_notebook.
 :::"""
 
 
-def test_examples(tmp_path):
+def test_examples(in_tmp_path):
     in_doc = fmt2fmt(MARKED_RMD, out_fmt='panflute')
     # We need to add the relevant metadata
     with pytest.raises(FilterError):
@@ -84,11 +87,19 @@ def test_examples(tmp_path):
     in_doc.metadata = {'noteout': {
         'book-url-root': 'https://resampling-stats.github.io/latest-r',
         'interact-url': "/interact/lab/index.html?path=",
+        'language': 'r',
         'nb-dir': "notebooks"}}
     with pytest.raises(FilterError, match='noteout.nb-format must be defined'):
         filter_doc(in_doc, anbL)
     # All, so now we check for the output notebook.
     in_doc.metadata['noteout']['nb-format'] = 'Rmd'
+    with pytest.raises(FilterError, match='" does not exist'):
+        filter_doc(in_doc, anbL)
+    # Write a fake notebook.
+    out_nb_path = Path('notebooks')
+    out_nb_path.mkdir()
+    (out_nb_path / 'a_notebook.Rmd').write_text('Some text')
+    # The filter now works.
     out_doc = filter_doc_nometa(in_doc, anbL)
     # When we don't know the output format (from the metadata), we get the
     # LaTeX version.
@@ -103,3 +114,79 @@ def test_examples(tmp_path):
     in_doc_nt.metadata = in_doc.metadata
     with pytest.raises(FilterError, match='title must be defined'):
         filter_doc(in_doc_nt, anbL)
+    # Try adding a data file to notebook.
+    data_path = Path('data')
+    data_path.mkdir()
+    in_data = data_path / 'df.csv'
+    in_data.write_text('a,b\n1,2\n3,4')
+    (out_nb_path / 'a_notebook.Rmd').write_text(
+        '''\
+```{r}
+df <- read.csv("data/df.csv")
+```''')
+    out_with_data = filter_doc_nometa(in_doc, anbL)
+    assert (out_nb_path / 'data').is_dir()
+    assert (out_nb_path / 'data' / 'df.csv').is_file()
+    assert (out_nb_path / 'a_notebook.zip').is_file()
+    data_rmd = (
+        HTML_RMD.replace(
+            'Download notebook',
+            'Download zip with notebook + data file')
+        .replace(
+            'notebooks/a_notebook.Rmd',
+            'notebooks/a_notebook.zip'))
+    assert fmt2md(out_with_data) == fmt2md(data_rmd)
+    # Two data files.
+    in_data = data_path / 'df2.csv'
+    in_data.write_text('a,b\n1,2\n3,4')
+    (out_nb_path / 'a_notebook.Rmd').write_text(
+        '''\
+```{r}
+df <- read.csv("data/df.csv")
+df2 <- read.csv("data/df2.csv")
+```''')
+    out_with_data2 = filter_doc_nometa(in_doc, anbL)
+    assert (out_nb_path / 'data' / 'df2.csv').is_file()
+    data_rmd = data_rmd.replace('+ data file', '+ data files')
+    assert fmt2md(out_with_data2) == fmt2md(data_rmd)
+
+
+def test_find_data_files(tmp_path):
+    nb = jpt.reads(
+        '''\
+```{r}
+df <- read.csv("data/df.csv")
+```''', 'Rmd')
+    assert anbL.find_data_files(nb) == ['data/df.csv']
+    # Can find multiple file reads in one cell.
+    nb = jpt.reads(
+        '''\
+```{r}
+df <- read.csv("data/df.csv")
+df2 <- read.csv("data/df2.csv")
+```''', 'Rmd')
+    assert anbL.find_data_files(nb) == ['data/df.csv', 'data/df2.csv']
+    # Can find matches in different cells.
+    nb = jpt.reads(
+        '''\
+```{r}
+df <- read.csv("data/df.csv")
+df2 <- read.csv("data/df2.csv")
+```
+
+Some text
+
+```{r}
+df2 <- read.csv("some_file.csv")
+```''', 'Rmd')
+    assert (anbL.find_data_files(nb) ==
+            ['data/df.csv', 'data/df2.csv', 'some_file.csv'])
+    # Matches are sorted and unique.
+    nb = jpt.reads(
+        '''\
+```{r}
+df2 <- read.csv("data/df2.csv")
+df <- read.csv("data/df.csv")
+df1 <- read.csv("data/df.csv")
+```''', 'Rmd')
+    assert anbL.find_data_files(nb) == ['data/df.csv', 'data/df2.csv']
