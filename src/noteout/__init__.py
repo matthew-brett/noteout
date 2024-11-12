267ba17f0db87afe4c43e087e92caa9678bb690c
Matthew Brett <matthew.brett@gmail.com>
2024-11-12 16:12:58 +0000

    Update version for new API

--- src/noteout/__init__.py
+++ src/noteout/__init__.py
@@ -1,7 +1,7 @@
 """ Notebook parsing filters for Pandoc
 """
 
-__version__ = '0.0.3a7'
+__version__ = '0.1.0a7'
 
 # Patch Panflute for Quarto RawBlocks:
 # https://github.com/sergiocorreia/panflute/pull/251
